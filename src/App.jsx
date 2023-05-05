c9c6ca5f71a25f9070488f3153df1c1a75802df0
psoper1 <psoper0604@gmail.com>
2023-05-05 09:58:18 -0400

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <>
+    <Home />
+    </>
+  );
+}
+
+export default App;
