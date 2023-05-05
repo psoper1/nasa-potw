3dddcd3f8d36aa58077a93b11b3204302490c244
psoper1 <psoper0604@gmail.com>
2023-05-05 14:57:56 -0400

    added

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a className="nav-link" aria-current="page" href="http://www.test.com">Home</a>
